f6bef214dfa66ab0dddf1f28b0a3164f2bdccf5c
maddonix <tlux14@googlemail.com>
2024-04-25 13:52:27 +0200

    Tests added

--- endoreg_client_manager/report_processor/apps.py
+++ endoreg_client_manager/report_processor/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class ReportProcessorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'report_processor'
---
+++ endoreg_client_manager/report_processor/tests/test_apps.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.apps import apps
+from ..apps import ReportProcessorConfig
+
+class ReportProcessorConfig(TestCase):
+    def test_apps_config_test(self):
+        print(dir(ReportProcessorConfig))  # This will print all attributes and methods
+        print(ReportProcessorConfig.name)  # This should print 'report_processor'
+        self.assertEqual(ReportProcessorConfig.name, 'report_processor')
+    def test_apps_config(self):
+        # Ensure that the AppConfig name is correct
+        self.assertEqual(ReportProcessorConfig.name, 'report_processor')
+        self.assertEqual(apps.get_app_config('report_processor').name, 'report_processor')
+
+        # Verify that the default auto field is set as expected
+        self.assertEqual(apps.get_app_config('report_processor').default_auto_field, 'django.db.models.BigAutoField')
